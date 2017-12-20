134c0c2a2947dec619a5b4017b8c0ac70e5af118
Rog_Z <izdev.404@gmail.com>
2017-12-20 15:04:55 +0800

    Virtual Env Removed

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hkerNews',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Kaung Myat Htet Khaing',
+    author_email='',
+    description='Hacker News RSS Feed Reader'
+)
